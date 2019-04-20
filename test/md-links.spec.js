f009f21cf2554b42a26d24587d0a4ebfc8881274
Nancy <lexyfeer@hotmail.com>
2019-04-19 19:20:20 -0500

    reparando la parte de tets ya corre

--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,4 +1,13 @@
-const mdLinks = require('../');
+const mdLinks = require('../mdlinks.js');
+
+describe ('pathInserted', () => {
+  it('Should return false', () => {
+    expect(mdLinks.pathInserted()).toBe (false);
+  });
+  it('Should return true', () => {
+    expect(mdLinks.pathInserted('./test.md')).toBe(true);
+  });
+})
 
 
 describe('mdLinks', () => {
